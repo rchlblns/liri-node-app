7df2403bca165d2b4aaedd59eae868492751cec3
Short Stack <41342213+rchlblns@users.noreply.github.com>
2018-10-19 15:45:58 -0500

    spotify-this-song working

--- liri.js
+++ liri.js
@@ -1,24 +1,36 @@
 require("dotenv").config();
-const keys = require("./keys.js");
-const request = require("request")
-const Spotify = require("node-spotify-api");
-var fs = require("fs");
+let keys = require("./keys.js");
+let request = require("request")
+let Spotify = require("node-spotify-api");
+let fs = require("fs");
 
-function getSpotifySong(songName) {
-    let spotify = new Spotify (keys.spotify); 
+let command = process.argv[2];
+let userInput = process.argv[3];
 
-    if (!songName) {
-        songName = "The Sign";
-    }
-    spotify.search({ type: "track", query: songName }, function(err, data) {
-        if (err) throw err;
-            console.log("An error occured" + err);
-
-    })
+switch(command) {
+    case "spotify-this-song":
+    spotifyThisSong();
+    break;
 }
 
+function spotifyThisSong() {
+    var spotify = new Spotify(keys.spotify);
+    spotify.search ({ type: "track", query: userInput, limit: 1 }, function (err, data) {
+        
+        if (err) {
+            console.log("Error occured");
+        }
+        else if (!userInput) {
+            userInput = "All Star";
+        }
+        else {
+            let songInfo = data.tracks.items[0];
+            console.log("Song: " + songInfo.name);
+            console.log("Artist: " + songInfo.album.artists[0].name);
+            console.log("Album: " +songInfo.album.name);
+            console.log("Preview song here: " + songInfo.album.external_urls.spotify);
 
+        }
 
-    
-
-
+    });
+}
